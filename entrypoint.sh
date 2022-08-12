f17b7737908e18a8be5901c2a53f4b67b98a605b
Alain Sirois <alainsirois@gmail.com>
2022-08-12 01:26:52 -0400

    added line to run without certificate

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,6 @@ bundle check || bundle install
 rm -f tmp/pids/server.pid
 
 bundle exec puma -b 'ssl://0.0.0.0:3000?key=/var/tmp/djalan.hopto.org/privkey.pem&cert=/var/tmp/djalan.hopto.org/fullchain.pem'
+
+# without certificate, defaults to port 9292
+# bundle exec puma
